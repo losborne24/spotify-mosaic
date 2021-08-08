23d83827f436fe1f170c5c249e424b3599daddde
LEITH OSBORNE <osborneleith@gmail.com>
2021-08-08 10:04:59 +0100

    added scopes

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const client_id = 'f1da744b65de4f5aaa1d3e3bb881d942';
 export const response_type = 'token';
 export const redirect_uri = 'http://localhost:3000/playlists';
-export const scopes = 'user-read-private user-read-email';
+export const scopes = 'user-top-read user-read-private user-read-email';
 export const access_token = 'access_token=';
