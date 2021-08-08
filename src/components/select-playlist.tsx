e869a41694d02811754a2a9f2a6b43fc0a7d7d11
LEITH OSBORNE <osborneleith@gmail.com>
2021-08-08 12:21:33 +0100

    added public playlists and personal top tracks

--- src/components/select-playlist.tsx
+++ src/components/select-playlist.tsx
@@ -8,7 +8,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/swiper.min.css';
 import 'swiper/components/navigation/navigation.min.css';
-import './custom-swiper.scss';
+import '../styles/custom-swiper.scss';
 
 import SwiperCore, { Navigation } from 'swiper/core';
 // install Swiper modules
@@ -16,6 +16,7 @@ SwiperCore.use([Navigation]);
 const Playlist = (props: any) => {
   const history = useHistory();
   const [personalPlaylists, setPersonalPlaylists] = useState([]) as any[];
+  const [publicPlaylists, setPublicPlaylists] = useState([]) as any[];
   const [inputPlaylistId, setInputPlaylistId] = useState('');
   const [offset, setOffset] = useState(0);
   const [isLoadMore, setLoadMore] = useState(false);
@@ -34,7 +35,6 @@ const Playlist = (props: any) => {
       .then((res: any) => {
         let _playlists: { img: any; id: any; name: any }[] = [];
         if (res.data?.items) {
-          console.log(res.data?.total);
           res.data.items.forEach((item: any) => {
             _playlists.push({
               img: item.images[0].url,
@@ -50,6 +50,36 @@ const Playlist = (props: any) => {
         }
         setOffset((offset) => (offset += 10));
         setPersonalPlaylists((playlists: any) => [...playlists, ..._playlists]);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  };
+  const fetchPublicPlaylists = () => {
+    axios
+      .get('https://api.spotify.com/v1/browse/categories/toplists/playlists', {
+        headers: { Authorization: `Bearer ${props?.token}` },
+        params: {
+          client_id: constants.client_id,
+          fields: 'items(name,images,id)',
+          response_type: constants.response_type,
+          limit: 10,
+          offset: 0,
+        },
+      })
+      .then((res: any) => {
+        let _playlists: { img: any; id: any; name: any }[] = [];
+        if (res.data?.playlists.items) {
+          res.data.playlists.items.forEach((item: any) => {
+            _playlists.push({
+              img: item.images[0].url,
+              id: item.id,
+              name: item.name,
+            });
+          });
+        }
+
+        setPublicPlaylists(_playlists);
       })
       .catch((err: any) => {
         console.log(err);
@@ -98,21 +128,66 @@ const Playlist = (props: any) => {
         console.log(err);
       });
   };
+  const fetchTopTracks = (period: String) => {
+    axios
+      .get('https://api.spotify.com/v1/me/top/tracks', {
+        headers: { Authorization: `Bearer ${props?.token}` },
+        params: {
+          time_range: period,
+          limit: 50,
+          offset: 0,
+        },
+      })
+      .then((res: any) => {
+        if (res.data?.items) {
+          const _tracks: { id: string; img: string; avgColour: any }[] = [];
+          const fac = new FastAverageColor();
+          const uniqueTracks: { id: string }[] = [];
+          res.data.items.forEach((item: any) => {
+            if (!uniqueTracks.find((u) => u.id === item.album.id)) {
+              uniqueTracks.push({ id: item.album.id });
+              fac.getColorAsync(item.album.images[2].url).then((color) => {
+                _tracks.push({
+                  id: item.album.id,
+                  img: item.album.images[2].url,
+                  avgColour: color.value,
+                });
+                // console.log(
+                //   '%c avgColour',
+                //   `color: rgba(${color.value[0]},${color.value[1]},${color.value[2]},${color.value[3]}`
+                // );
+              });
+            }
+          });
+          props.setTracks(_tracks);
+          history.push('/selectImage');
+        }
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
     props.setToken(
       window.location.hash.split(constants.access_token).pop()?.split('&')[0]
     );
   }, []);
   useEffect(() => {
-    if (props.token) fetchPersonalPlaylists();
+    if (props.token) {
+      fetchPersonalPlaylists();
+      fetchPublicPlaylists();
+    }
   }, [props.token]);
   const useStyles = makeStyles({
     marginLeft: { marginLeft: '0.5rem' },
     marginRight: { marginRight: '0.5rem' },
-    width: { width: '100%' },
+    width100: { width: '100%' },
+    widthXL: { width: '83%' },
+    widthXS: { width: '17%' },
     playlistImage: {
-      maxHeight: '15vw',
-      maxWidth: '15vw',
+      maxHeight: '14vw',
+      maxWidth: '14vw',
     },
     playlistInputContainer: {
       display: 'flex',
@@ -132,15 +207,98 @@ const Playlist = (props: any) => {
     },
     loadMore: {
       border: '0.1rem solid black',
-      height: '15vw',
-      width: '15vw',
+      height: '14vw',
+      width: '14vw',
     },
+    playlistContainer: {
+      display: 'flex',
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    publicContainer: {
+      flexDirection: 'column',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '83%',
+    },
+    topTracksContainer: {
+      flexDirection: 'column',
+      display: 'flex',
+      justifyContent: 'center',
+      textAlign: 'center',
+      alignItems: 'center',
+      width: '17%',
+      height: '100%',
+    },
+    btnTopTrackContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      height: '100%',
+    },
+    btnTopTracks: { width: '100%', margin: '0.5rem' },
   });
   const classes = useStyles();
+  const yourTopTrackStrings = [
+    { id: 'short_term', text: 'Short Term' },
+    { id: 'medium_term', text: 'Medium Term' },
+    { id: 'long_term', text: 'Long Term' },
+  ];
   return (
     <>
-      <h2>Personal Playlists</h2>
-      <Swiper slidesPerView={6} navigation={true} className={classes.width}>
+      <div className={classes.playlistContainer}>
+        <div className={`${classes.publicContainer} `}>
+          <h2>Public Playlists</h2>
+          <Swiper
+            slidesPerView={5}
+            navigation={true}
+            className={classes.width100}
+          >
+            {publicPlaylists.map((item: any, index: any) => {
+              return (
+                <SwiperSlide
+                  className={`${classes.swiperSlide}`}
+                  key={`swiper-slider-${index} `}
+                  onClick={() => {
+                    setInputPlaylistId(item.id);
+                  }}
+                >
+                  <img
+                    className={`${classes.playlistImage}`}
+                    key={`img-${index}`}
+                    src={item.img}
+                    alt="album cover"
+                  ></img>
+                  <p className={classes.txtFlex}>{item.name}</p>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+        <div className={`${classes.topTracksContainer} `}>
+          <h2>Your Top Tracks</h2>
+          <div className={classes.btnTopTrackContainer}>
+            {yourTopTrackStrings.map((item: any, index: any) => {
+              return (
+                <Button
+                  className={`${classes.btnTopTracks}`}
+                  variant="contained"
+                  color="primary"
+                  onClick={() => {
+                    fetchTopTracks(item.id);
+                  }}
+                >
+                  {item.text}
+                </Button>
+              );
+            })}
+          </div>
+        </div>{' '}
+      </div>{' '}
+      <h2>Your Playlists</h2>
+      <Swiper slidesPerView={6} navigation={true} className={classes.width100}>
         {personalPlaylists.map((item: any, index: any) => {
           return (
             <SwiperSlide
@@ -173,7 +331,7 @@ const Playlist = (props: any) => {
         ) : (
           <></>
         )}
-      </Swiper>
+      </Swiper>{' '}
       <div className={classes.playlistInputContainer}>
         <TextField
           label="Enter Playlist ID"
