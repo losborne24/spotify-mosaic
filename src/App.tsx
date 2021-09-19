9c7400e0440e77a512cf0f06356a271f8bdc0784
LEITH OSBORNE <osborneleith@gmail.com>
2021-09-19 18:29:26 +0100

    switched to ga4 + browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Playlist from './components/select-playlist';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Mosaic from './components/mosaic';
 import ConnectToSpotify from './components/connect-to-spotify';
 import { makeStyles } from '@material-ui/core';
 import SelectImage from './components/select-image';
 import * as constants from './constants';
-import Analytics from 'react-router-ga';
+import ReactGA from 'react-ga4';
+import { createBrowserHistory } from 'history';
 
+export const history = createBrowserHistory();
+history.listen((location) => {
+  console.log(location);
+  ReactGA.send(location);
+});
 const App = () => {
   const [imageSrc, setImageSrc] = useState<any>();
   const [fetchMoreUrl, setFetchMoreUrl] = useState<string | null>(null);
@@ -21,7 +27,9 @@ const App = () => {
   const createImg = (imageSrc: any) => {
     setImageSrc(imageSrc);
   };
-
+  useEffect(() => {
+    ReactGA.initialize('G-JQQCW8E695');
+  }, []);
   const useStyles = makeStyles({
     center: {
       display: 'flex',
@@ -35,57 +43,44 @@ const App = () => {
   const classes = useStyles();
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Analytics id="G-JQQCW8E695" debug>
-        <Switch>
-          <Route path={constants.select_playlist_url}>
-            <div className={classes.center}>
-              <Playlist
-                setToken={setToken}
-                token={token}
-                setTracks={setTracks}
-                returnToMosaic={returnToMosaic}
-                setFetchMoreUrl={setFetchMoreUrl}
-                setUniqueTracks={setUniqueTracks}
-              />
-            </div>
-          </Route>
-          <Route path="/:access_token(access_token=.*)">
-            <div className={classes.center}>
-              <Playlist
-                setToken={setToken}
-                token={token}
-                setTracks={setTracks}
-                returnToMosaic={returnToMosaic}
-                setFetchMoreUrl={setFetchMoreUrl}
-                setUniqueTracks={setUniqueTracks}
-              />
-            </div>
-          </Route>
-          <Route path={constants.select_image_url}>
-            <div className={classes.center}>
-              <SelectImage createImg={(src: any) => createImg(src)} />
-            </div>
-          </Route>
-          <Route path={constants.create_mosaic_url}>
-            <Mosaic
-              fetchMoreUrl={fetchMoreUrl}
-              setFetchMoreUrl={setFetchMoreUrl}
+      <Switch>
+        <Route path={constants.select_playlist_url}>
+          <div className={classes.center}>
+            <Playlist
+              setToken={setToken}
               token={token}
-              tracks={tracks}
-              imageSrc={imageSrc}
-              setReturnToMosaic={setReturnToMosaic}
-              setUniqueTracks={setUniqueTracks}
-              uniqueTracks={uniqueTracks}
               setTracks={setTracks}
+              returnToMosaic={returnToMosaic}
+              setFetchMoreUrl={setFetchMoreUrl}
+              setUniqueTracks={setUniqueTracks}
             />
-          </Route>
-          <Route path="/">
-            <div className={classes.center}>
-              <ConnectToSpotify />
-            </div>
-          </Route>
-        </Switch>{' '}
-      </Analytics>
+          </div>
+        </Route>
+
+        <Route path={constants.select_image_url}>
+          <div className={classes.center}>
+            <SelectImage createImg={(src: any) => createImg(src)} />
+          </div>
+        </Route>
+        <Route path={constants.create_mosaic_url}>
+          <Mosaic
+            fetchMoreUrl={fetchMoreUrl}
+            setFetchMoreUrl={setFetchMoreUrl}
+            token={token}
+            tracks={tracks}
+            imageSrc={imageSrc}
+            setReturnToMosaic={setReturnToMosaic}
+            setUniqueTracks={setUniqueTracks}
+            uniqueTracks={uniqueTracks}
+            setTracks={setTracks}
+          />
+        </Route>
+        <Route path="/">
+          <div className={classes.center}>
+            <ConnectToSpotify />
+          </div>
+        </Route>
+      </Switch>
     </Router>
   );
 };
--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@ export const client_id = 'f1da744b65de4f5aaa1d3e3bb881d942';
 export const response_type = 'token';
 export const redirect_uri =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000/'
-    : 'https://losborne24.github.io/spotify-mosaic/';
+    ? 'http://localhost:3000/playlists'
+    : 'https://losborne24.github.io/playlists';
 export const scopes = 'user-top-read user-read-private user-read-email';
 export const access_token = 'access_token=';
 export const state_res = 'state=';
